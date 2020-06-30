3c9abfeac1d0563a61bf89ba0731dff160b68591
J-azuma <azumanotdetail@gmail.com>
2020-06-30 15:44:23 +0900

    (1-2) 管理者登録時の入力値チェックを実装。エラー発生時にフォームが真っ赤になる仕様を削除

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,13 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,11 +16,18 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message="名前を入力してください")
+	@Size(min=1, max=20, message="名前は1文字以上 20文字以内で入力してください")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="メールアドレスを入力してください")
+	@Email(message="正しい形式で入力してください")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message="パスワードを入力してください")
+	@Size(min=8, max=16, message="パスワードは8文字以上16文字以内で入力してください")
 	private String password;
+	
 	public String getName() {
 		return name;
 	}
