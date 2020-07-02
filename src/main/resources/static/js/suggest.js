d7e0ce6dd61731f2341048f4f3cac60401df5e48
J-azuma <azumanotdetail@gmail.com>
2020-07-02 11:23:28 +0900

    (6-4)検索機能にオートコンプリートを実装

---
+++ src/main/resources/static/js/suggest.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+$(function() {
+	$("#search-name").on("keyup", function() {
+			var hostUrl = "http://localhost:8080/employee/suggest";
+			$.ajax({
+				url: hostUrl,
+				type: "POST",
+				datatype: "json",
+			async: true
+			}).done(function(data) {
+				console.log(data);
+				    $( "#search-name" ).autocomplete({
+				      source: data.employeeNameArray
+				    });
+			}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+				console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+				console.log("textStatus : " + textStatus); 
+				console.log("errorThrown : " + errorThrown.message);
+			});
+	  });
+})
