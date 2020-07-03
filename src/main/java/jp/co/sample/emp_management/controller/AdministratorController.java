c6b4028496179535339db296a707ccea88a3d133
J-azuma <azumanotdetail@gmail.com>
2020-07-03 16:18:44 +0900

    (5-3) パスワードのハッシュ化を実装

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -33,6 +34,7 @@ public class AdministratorController {
 	
 	@Autowired
 	private HttpSession session;
+	
 
 	/**
 	 * 使用するフォームオブジェクトをリクエストスコープに格納する.
--- src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
+++ src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
@@ -1,6 +1,7 @@
 package jp.co.sample.emp_management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,12 +21,15 @@ public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	/**
 	 * 管理者情報を登録します.
 	 * 
 	 * @param administrator　管理者情報
 	 */
 	public void insert(Administrator administrator) {
+		administrator.setPassword(encoder.encode(administrator.getPassword()));
 		administratorRepository.insert(administrator);
 	}
 	
@@ -35,9 +39,12 @@ public class AdministratorService {
 	 * @param password パスワード
 	 * @return 管理者情報　存在しない場合はnullが返ります
 	 */
-	public Administrator login(String mailAddress, String passward) {
-		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, passward);
-		return administrator;
+	public Administrator login(String mailAddress, String password) {
+		Administrator administrator = administratorRepository.findByMailAddress(mailAddress);
+		if (encoder.matches(password, administrator.getPassword())){
+			return administrator;
+		}
+		return null;
 	}
 	
 	public Administrator findByMailAddress(String mailAddress) {
